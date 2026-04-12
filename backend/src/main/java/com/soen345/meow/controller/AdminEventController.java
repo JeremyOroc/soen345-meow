a46c4a85118da6fe99a4e986b152330773acfd10
Ahmad Saadawi <ahmad.saadawi7@gmail.com>
2026-04-11 22:23:43 -0400

    fix production cors for vercel and preflight

--- backend/src/main/java/com/soen345/meow/controller/AdminEventController.java
+++ backend/src/main/java/com/soen345/meow/controller/AdminEventController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/api/admin/events")
 public class AdminEventController {
